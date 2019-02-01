a8a7fbd6980bb9b4ec406449b6b54ba24e81c8eb
zoheiry <ali.elzoheiry@gmail.com>
2019-02-01 13:27:27 +0100

    Modify default template

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h3>Looks like you are about to make greatness happen.</h3>
+          <p>Looks like you are about to make greatness happen.</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
