8180ce389741f885f80e0fccff6788cf92bcee36
zoheiry <ali.elzoheiry@gmail.com>
2019-02-25 10:08:09 +0100

    Create redux store and move some dependencies to dev dependencies

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
+import createStore from './createStore';
 
 Sentry.init({
   dsn: fashionTradeSettings.sentry_url,
@@ -10,4 +11,5 @@ Sentry.init({
   environment: fashionTradeSettings.environment,
 });
 
-ReactDOM.render(<App />, document.getElementById('ft-app'));
+const store = createStore();
+ReactDOM.render(<App store={store} />, document.getElementById('ft-app'));
