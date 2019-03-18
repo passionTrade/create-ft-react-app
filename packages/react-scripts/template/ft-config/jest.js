c251ca59550c08207760abe15af8d2f7cf459ae3
zoheiry <ali.elzoheiry@gmail.com>
2019-03-18 16:30:32 +0100

    fix svg files in test

--- packages/react-scripts/template/ft-config/jest.js
+++ packages/react-scripts/template/ft-config/jest.js
@@ -11,6 +11,8 @@ module.exports = {
   moduleNameMapper: {
     '^.+\\.(css|scss)$': 'babel-jest',
     'react-i18next': '<rootDir>/__mocks__/react-i18next.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file.js',
   },
   setupFiles: ['<rootDir>/ft-config/test-setup.js'],
   setupTestFrameworkScriptFile: 'jest-enzyme',
